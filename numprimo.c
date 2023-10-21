0ab9c18638fae6dbb63db8b68cbde6eb986ae87d
Martín Burga <martin.burga@udla.edu.ec>
2023-10-21 18:25:56 -0500

    II): Se estructura el proceso de calculo de si un numero es primo o no.

--- numprimo.c
+++ numprimo.c
@@ -11,8 +11,21 @@ int main(){
     {
         printf("Por favor, inserte un numero entero positivo.\n");
         scanf("%d", &numero);
+        esprimo=1;
+        if (numero>1)
+        {
+            for (int i = 2; i <= numero/2; i++)
+            {
+                if (numero%1==0)
+                {
+                    esprimo=0;
+                }
+                
+            }
+            
+        }
+        
     } while (numero>0);
-    
-  
+
     return 0;
 }
