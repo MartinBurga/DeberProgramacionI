3488047a32bb585eab54538caa0f5441a7b855ef
Martín Burga <martin.burga@udla.edu.ec>
2023-10-21 18:56:39 -0500

    IV): Por ultimo, se le agregan algunos controladores ante posibles ingresos de numeros negativos y una opcion de fin algoritmo

--- numprimo.c
+++ numprimo.c
@@ -9,27 +9,51 @@ int main(){
 
     do
     {
-        printf("Por favor, inserte un numero entero positivo.\n");
-        scanf("%d", &numero);
-        esprimo=1;
-        if (numero>1)
+        do
         {
-            for (int i = 2; i <= numero/2; i++)
+            printf("Por favor, inserte un numero entero positivo.\n");
+            printf("Inserta ""0"" si deseas salir\n");
+            scanf("%d", &numero);
+            if (numero<0)
             {
-                if (numero%1==0)
-                {
-                    esprimo=0;
-                } 
-            }  
-        }
-        if (esprimo)
+                printf("El numero insertado debe ser un entero positivo. Vuelve a intentarlo.\n");
+                printf("|--------------------------------------------|\n");
+            }
+            
+        } while (numero<0);
+
+        if (numero==0)
         {
-            printf("El numero %d si es un numero primo.\n", numero);
-        } else 
+            printf("Gracias por utilizar el software! Nos vemos.\n");
+            printf("|--------------------------------------------|\n");
+        } else if (numero==1)
         {
-            printf ("El numero %d NO es un numero primo.\n", numero);
+            printf("El numero 1 NO es un numero primo.\n");
+            printf("|--------------------------------------------|\n");
         }
         
+        if (numero>1)
+        {
+            esprimo=1;
+            for (int i = 2; i <= numero/2; i++)
+            {
+                if (numero%i==0)
+                {
+                    esprimo=0;
+                    break;
+                } 
+            }
+            if (esprimo)
+            {
+            printf("El numero %d SI es un numero primo.\n", numero);
+            printf("|--------------------------------------------|\n");
+            } else
+            {
+            printf ("El numero %d NO es un numero primo.\n", numero);
+            printf("|--------------------------------------------|\n");
+            }
+        }
+  
     } while (numero>0);
 
     return 0;
