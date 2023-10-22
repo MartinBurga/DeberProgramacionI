e9abc17940694fa20ebf6dc34bcce29e5b87b366
Martín Burga <martin.burga@udla.edu.ec>
2023-10-21 21:00:36 -0500

    II): Se realiza la estructura del calculo. Basicamente se multiplica el numero insertado por una variable i que esta empieza en 1 y conforme se multiplique va aumentando 1 hasta llegar al 10.

--- Tablamultiplicacion.c
+++ Tablamultiplicacion.c
@@ -6,12 +6,18 @@ int producto;
 
 int main(){
     printf("Bienvenido a la calculadora de tablas de multiplicacion.\n");
-    while (numero<1 || numero>0)
+    while (numero<1 || numero>10)
     {
         printf("Por favor, inserta un numero entre 1-10 para determinar su respectiva tabla de multiplicacion\n");
         scanf("%d", &numero);  
     }
     
+    printf("La tabla de %d es:\n", numero);
+    for (int i = 1; i <= 10; i++)
+    {
+        producto=numero*i;
+        printf("%d x %d = %d\n", numero, i, producto);
+    }
     
     return 0;
 }
