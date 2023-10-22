81c7bb21eaf51dbf411fda930ddf86278cdbac70
Martin Burga <martin.burga@udla.edu.ec>
2023-10-22 01:53:33 -0500

    I): Estructura del menu y declaracion de variables con las que se van a trabajar.

---
+++ primomayor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int num1, num2;
+int contador1, contador2;
+int opcion;
+
+int main(){
+    printf("Bienvenido al comparador de numeros primos por digitos.\n");
+    do
+    {
+        printf("Inserta un primer numero entero.");
+        scanf("%d", &num1);
+        printf("Inserta un segundo numero entero.");
+        scanf("%d", &num2);
+
+    } while (opcion==1);
+    
+    
+    return 0;
+}
