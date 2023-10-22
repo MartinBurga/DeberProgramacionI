c0b1173d7caa9d3c1557d3b01ddf599c94b515bc
Martin Burga <martin.burga@udla.edu.ec>
2023-10-22 02:08:04 -0500

    III): Se repite la misma estructura y calculo para el analisis del segundo numero. Se separa el termino, se lo analiza, se comprueba y se pasa al siguiente digito.

--- primomayor.c
+++ primomayor.c
@@ -21,12 +21,29 @@ int main(){
             {
                 contador1++;
             }
-            
+            num1=num1/10;
         }
         
-        //Basicamente el proceso que se hace aqui es que con el "%10" se va como, por decirlo de una forma,
+        // Basicamente el proceso que se hace aqui es que con el "%10" se va como, por decirlo de una forma,
         // "separando los terminos" y con el comando de if es que se determina si es un numero primo. 
-        //Si es que es un numero primo, entonces se suma 1 al contador.
+        // Si es que es un numero primo, entonces se suma 1 al contador. La variable x1 y x2 son almacenadores
+        // temporales de los numeros que su funcion es unicamente la de guardar el numero para comparar si es
+        // primo o no. 
+
+        contador2=0;
+        while (num2>0)
+        {
+            int x2=num2%10;
+            if (x2==2 || x2==3 || x2==5 || x2==7)
+            {
+                contador2++;
+            }
+            num2=num2/10;
+        }
+
+        // Mismo proceso y estructura pero ahora con el contador 2 que pertenece al 2do numero insertado por
+        // el usuario. Se le divide para 10 para una vez que se haya comprobado si es primo o no, pase al siguiente
+        // termino del numero entero. 
 
     } while (opcion==1);
     
