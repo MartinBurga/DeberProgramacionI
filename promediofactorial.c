4d6a8b13bb8cad9ba07461e4052bce0c45f537ae
Martin Burga <martin.burga@udla.edu.ec>
2023-10-22 00:11:15 -0500

    Estructura del calculo de factorial + calculo de la cantidad de factoriales. El for de i se encarga de contar la cantidad de factoriales mientras que el segundo for se encarga de calcular la multiplicacion de cada factorial con el numero insertado por el usuario.

--- promediofactorial.c
+++ promediofactorial.c
@@ -4,6 +4,8 @@
 int numero;
 int factorial;
 int promedio;
+int suma;
+int numfactorial;
 
 int main(){
     do
@@ -11,6 +13,20 @@ int main(){
         printf("Bienvenido a la calculadora de promedio de factoriales.\n");
         printf("Por favor, inserta un numero entero positivo\n");
         scanf("%d", &numero);
+
+        for (int i = 1; i <= numero; i++)
+        {
+            factorial=1;
+            for (int a = i; a <=i; a++)
+            {
+                factorial=factorial*a;
+                suma=suma+factorial;
+                numfactorial++;
+            }
+            
+            
+        }
+        
 
     } while (numero<=0);
     
