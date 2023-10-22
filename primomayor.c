d520584a19bbe736d27449e0177361e458dbd1c0
Martin Burga <martin.burga@udla.edu.ec>
2023-10-22 02:02:25 -0500

    II): Como se especifica en el comentario, es que a base del %10 es que se separa los terminos y se determina si es primo o no. Si lo es, se suma 1 al contador 1.

--- primomayor.c
+++ primomayor.c
@@ -9,10 +9,24 @@ int main(){
     printf("Bienvenido al comparador de numeros primos por digitos.\n");
     do
     {
-        printf("Inserta un primer numero entero.");
+        printf("Inserta un primer numero entero.\n");
         scanf("%d", &num1);
-        printf("Inserta un segundo numero entero.");
+        printf("Inserta un segundo numero entero.\n");
         scanf("%d", &num2);
+        contador1=0;
+        while (num1>0)
+        {
+            int x1=num1%10;
+            if (x1==2 || x1==3 || x1==5 || x1==7)
+            {
+                contador1++;
+            }
+            
+        }
+        
+        //Basicamente el proceso que se hace aqui es que con el "%10" se va como, por decirlo de una forma,
+        // "separando los terminos" y con el comando de if es que se determina si es un numero primo. 
+        //Si es que es un numero primo, entonces se suma 1 al contador.
 
     } while (opcion==1);
     
