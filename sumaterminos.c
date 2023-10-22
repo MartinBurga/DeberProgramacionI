7f28cfe2b1ef7b23a7e4f89863525a3e20cad164
Martin Burga <martin.burga@udla.edu.ec>
2023-10-22 02:55:27 -0500

    V): ACTUALIZACION. Adicion de comentarios en sitios importantes del codigo.

--- sumaterminos.c
+++ sumaterminos.c
@@ -20,6 +20,9 @@ int main(){
             }
             printf("La suma de todos los terminos comprendidos entre 1 y el numero insertado es de: %d\n", respuesta);
             printf("|.---------------------------------------------------------.|\n");
+            // Si el numero es positivo, se procederá sumar la respuesta que es 0, al numero insertado.
+            // Una vez sumado se le restará 1 al numero insertado y la variable de "respuesta" adquiere
+            // el valor previamente calculado. Nuevamente se hara la suma y se restará otra vez 1 al numero.
 
         } else if (numero<0)
         {
@@ -30,6 +33,8 @@ int main(){
             }
             printf("La suma de todos los terminos comprendidos entre 1 y el numero insertado es de: -%d\n", respuesta);
             printf("|.---------------------------------------------------------.|\n");
+            //Mismo proceso y calculo que en la parte de arriba solo que en este se calcula la suma 
+            // de valores negativos dando la oportunidad de sumar tanto numero positivos como negativos.
         }
         
         printf("Desearias volver a calcular la suma de terminos?\n");
@@ -37,6 +42,8 @@ int main(){
         printf("0. No, es suficiente.\n");
         scanf("%d", &numero);
         printf("|.---------------------------------------------------------.|\n");
+        //Finalmente, se le pregunta al usuario si desearia volver a calcular la suma de los terminos de un entero
+        // o si desea poner fin al algoritmo.
 
     } while (numero!=0);
     
