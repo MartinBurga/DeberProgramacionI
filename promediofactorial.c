284156e10ead52bb60d47190dd6023168e70ea22
Martin Burga <martin.burga@udla.edu.ec>
2023-10-21 22:56:59 -0500

    I): Estructura base del menu con sentencia repetitiva do/while.

---
+++ promediofactorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int numero;
+int factorial;
+int promedio;
+
+int main(){
+    do
+    {
+        printf("Bienvenido a la calculadora de promedio de factoriales.\n");
+        printf("Por favor, inserta un numero entero positivo\n");
+        scanf("%d", &numero);
+
+    } while (numero<=0);
+    
+    
+
+}
