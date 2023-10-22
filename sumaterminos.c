84cb42ab71bbaea7b2c83279a8f93b31ba0bd3b4
Martin Burga <martin.burga@udla.edu.ec>
2023-10-21 20:12:00 -0500

    IV): Finalmente, se agrega un pequeño menu en el cual pregunta al usuario si desea volver a calcular o no. Correcion de errores y estetica.

--- sumaterminos.c
+++ sumaterminos.c
@@ -19,6 +19,7 @@ int main(){
                 numero--;
             }
             printf("La suma de todos los terminos comprendidos entre 1 y el numero insertado es de: %d\n", respuesta);
+            printf("|.---------------------------------------------------------.|\n");
 
         } else if (numero<0)
         {
@@ -28,9 +29,17 @@ int main(){
                 numero++;
             }
             printf("La suma de todos los terminos comprendidos entre 1 y el numero insertado es de: -%d\n", respuesta);
+            printf("|.---------------------------------------------------------.|\n");
         }
         
+        printf("Desearias volver a calcular la suma de terminos?\n");
+        printf("1. Si por favor.\n");
+        printf("0. No, es suficiente.\n");
+        scanf("%d", &numero);
+        printf("|.---------------------------------------------------------.|\n");
+
     } while (numero!=0);
     
+    printf("Gracias por utilizar el software! Nos vemos.\n");
     return 0;
 }
